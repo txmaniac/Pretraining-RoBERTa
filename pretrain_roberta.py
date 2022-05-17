7fd875a7f3aac605b4c8f54168bf7b9d7ada512b
Sreecharan Bojja <1756491@tcs.com>
2022-05-17 21:02:13 +0530

    Prepared YAML and code file for pretraining on PrivaSeer dataset

--- pretrain_roberta.py
+++ pretrain_roberta.py
@@ -14,17 +14,19 @@ import sys
 
 if __name__ == "__main__":
 
-    dir_path = sys.argv[1]
-    model_path = sys.argv[2]
-    logging_path = sys.argv[3]
-    output_path = sys.argv[4]
+    train_dir_path = sys.argv[1]
+    eval_dir_path = sys.argv[2]
+    model_path = sys.argv[3]
+    logging_path = sys.argv[4]
+    output_path = sys.argv[5]
 
     tokenizer = RobertaTokenizer.from_pretrained(model_path)
     config = RobertaConfig.from_pretrained(model_path)
 
     model = RobertaForMaskedLM.from_pretrained(model_path, config=config)
 
-    train_dataset = read_dataset(dir_path, model_path)
+    train_dataset = read_dataset(train_dir_path, model_path)
+    eval_dataset = read_dataset(eval_dir_path, model_path)
 
     data_collator = DataCollatorForLanguageModeling(
         mlm=True,
@@ -36,7 +38,7 @@ if __name__ == "__main__":
     training_args = TrainingArguments(
         do_train=True,
         output_dir = output_path,
-        per_device_train_batch_size=16,
+        per_device_train_batch_size=8,
         learning_rate=6e-4,
         warmup_steps=30000,
         adam_epsilon=1e-6,
@@ -52,7 +54,8 @@ if __name__ == "__main__":
         model = model,
         args = training_args,
         data_collator = data_collator,
-        train_dataset = train_dataset
+        train_dataset = train_dataset,
+        eval_dataset = eval_dataset
     )
 
     print('Training starts...')
