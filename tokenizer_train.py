59178d1ced385d0d3d704fc982852f5bfba37c56
Sreecharan Bojja <1756491@tcs.com>
2022-05-25 22:05:56 +0530

    Training tokenzier

--- tokenizer_train.py
+++ tokenizer_train.py
@@ -1,32 +1,28 @@
-from tokenizers import Tokenizer
-from tokenizers.models import BPE
-from tokenizers.trainers import BpeTrainer
-from tokenizers.pre_tokenizers import Whitespace
-
 import os
 from tqdm import tqdm
 import sys
+from transformers import RobertaTokenizer
+from datasets import load_dataset
 
 if __name__ == "__main__":
     list_of_args = sys.argv[1:]
 
-    dataset_path = list_of_args[0]
-    tokenizer_save_path = list_of_args[1]
-    paths = []
-
-    for i in tqdm(range(1,102), desc='Reading directories'):
-        path = os.path.join(dataset_path, str(i))
-        list_of_files = os.listdir(path)
-
-        for file in list_of_files:
-            paths.append(os.path.join(path, file))
-
-    tokenizer = Tokenizer(BPE(unk_token="<unk>"))
-    trainer = BpeTrainer(special_tokens=["<s>","<pad>","</s>","<unk>","<mask>"])
-
-    tokenizer.pre_tokenizer = Whitespace()
+    path = list_of_args[0]
+    dataset_path = list_of_args[1]
+    model_path = list_of_args[2]
     
-    tokenizer.train(paths, trainer)
-    
+    # paths = []
+
+    # for i in tqdm(range(1,102), desc='Reading directories'):
+    #     path = os.path.join(dataset_path, str(i))
+    #     list_of_files = os.listdir(path)
+
+    #     for file in list_of_files:
+    #         paths.append(os.path.join(path, file))
+
+    dataset = load_dataset('text', data_files=path, split='train', streaming=True)
+
+    old_tokenizer = RobertaTokenizer.from_pretrained(model_path)
+    new_tokenizer = old_tokenizer.train_new_from_iterator(dataset, 30000)
     #Save the Tokenizer to disk
-    tokenizer.save_pretrained(os.path.join(tokenizer_save_path))
+    new_tokenizer.save_pretrained(os.path.join(model_path))
