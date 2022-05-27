45ba9f36873b8cd4482ecaf01e2c3f47fa6a5b86
Sreecharan Bojja <1756491@tcs.com>
2022-05-27 13:52:26 +0530

    Prepped code for tokenizer training

--- tokenizer_training.py
+++ tokenizer_training.py
@@ -1,9 +1,13 @@
-from transformers import RobertaTokenizerFast
+from transformers import RobertaForMaskedLM, RobertaTokenizerFast
 from datasets import load_dataset
 import sys
 
 data_path = sys.argv[1]
+model_path = sys.argv[2]
+
 old_tokenizer = RobertaTokenizerFast.from_pretrained('roberta-base')
+model = RobertaForMaskedLM.from_pretrained('roberta-base')
+
 dataset = load_dataset('text', data_files=data_path, split='train')
 
 batch_size = 1000
@@ -15,4 +19,5 @@ def batch_iterator():
 
 new_tokenizer = old_tokenizer.train_new_from_iterator(batch_iterator(), vocab_size=30000)
 
-new_tokenizer.save_pretrained()
+model.save_pretrained(model_path)
+new_tokenizer.save_pretrained(model_path)
